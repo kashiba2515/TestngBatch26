2a8161d4580a2ec30bb7bdc92c4e330e84045ae1
user <user@DESKTOP-VDQ31QR>
2020-06-29 00:46:33 -0700

    cjc

---
+++ GitDemo2/src/git2/Demo2.java
@@ -0,0 +1,13 @@
+package git2;
+
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Local Git2");
+		System.out.println("GIT");
+		System.out.println("start Git");
+		
+	}
+
+}
